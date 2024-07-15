5c084741c3191248f355a4fe0d57e24ee5e900ea
Project Platform - Outsourcing <projectplatform-outsourcing@Obaretins-MacBook-Air.local>
2024-07-15 14:02:21 +0100

    changes was made to the staff appraisal pertern

--- src/Component/Appraisal_button.js/appraise.js
+++ src/Component/Appraisal_button.js/appraise.js
@@ -1,10 +1,140 @@
 
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import { useAppDispatch, useAppSelector } from "../../hook/hook";
+// import { updateScore } from "../../Slice/AppraiseSlice";
+// import { useNavigate } from "react-router-dom";
+// import Navbar from "../NavBar";
+// import './Appraise.css';
+
+// const Appraisal = () => {
+//   const user = useAppSelector((state) => state.user.user);
+//   const dispatch = useAppDispatch();
+//   const navigate = useNavigate();
+//   const [staffList, setStaffList] = useState([]);
+//   const [appraisalGroup, setAppraisalGroup] = useState([]);
+//   const [sqaapp, setSqaapp] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://uba-outsourced.onrender.com/v1/staff');
+//         const AppraisedStaff = await axios.get('https://uba-outsourced.onrender.com/v1/staff/apppraisal')
+//         const sqa = await axios.get('https://uba-outsourced.onrender.com/v1/AppraisalUpload')
+//         console.log('this is for appraised staff',AppraisedStaff.data.AppraisalList)
+//         console.log(sqa)
+//         const staff = response.data.staffList;
+//         const AppraisalList = AppraisedStaff.data.AppraisalList;
+//         const filteredStaff = staff.filter((item) => item.supervisorId === user.id.toUpperCase());
+//         const filteredAppraisal = AppraisalList.filter((item) => item.supervisorId === user.id.toUpperCase());
+//         const sqaAppraised = sqa.data.staffList
+//             console.log('na im be this',sqaAppraised)
+//         setStaffList(filteredStaff)
+//         setAppraisalGroup(filteredAppraisal)
+//         console.log(filteredAppraisal)
+//         setSqaapp(sqaAppraised)
+       
+
+//       } catch (error) {
+//         console.error("Error fetching staff data:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [user.id]); // Fetch data whenever user id changes
+
+// // navigate to the  major appraisal page
+// const handleStaffDetails = (staffId) => {
+//   console.log(staffId)
+//   navigate(`/staff/${staffId}`);
+// };
+
+// // the appraised staff 
+
+
+
+//   return (
+//     <>
+//       {/* <Navbar /> */}
+//       <div className="container">
+//         <div className="card">
+//       <div className="staff pb-3">List of Staff</div>
+//       <table className="table table-striped tab ">
+//             <thead className="thead-dark" >
+//               <tr>
+//                 <th>STAFF ID</th>
+//                 <th>FIRST NAME</th>
+//                 <th>LAST NAME</th>
+//                 <th>SOL ID</th>
+                
+//                 <th>Appraise Staff</th>
+//               </tr>
+//             </thead>
+//           {staffList.map((staff) => (
+     
+//             <tr key={staff.staffId} className="warning"> 
+//               <td>{staff.staffId}</td>
+//               <td>{staff.firstname}</td>
+//               <td>{staff.lastname}</td>
+//               <td>{staff.solId}</td>
+//               <td>
+//                 <button className="btn btn-sm appbtn btn-primary" onClick={() => handleStaffDetails(staff.staffId)}>
+//                   Update Score
+//                 </button>
+
+
+//               </td>
+//             </tr>
+         
+//           ))}
+//    </table>
+//    </div>
+//    </div>
+
+// <div className="container">
+//   <div className="card">
+//    <div className="staff pb-3">List of appraised Staff</div>
+//       <table className="table table-striped tab ">
+//             <thead className="thead-dark" >
+//               <tr>
+//                 <th>STAFF ID</th>
+//                 <th>FIRST NAME</th>
+//                 <th>LAST NAME</th>
+//                 <th>SOL ID</th>
+//                 <th>supervisorId</th>
+               
+//                 <th>Score</th>
+//               </tr>
+//             </thead>
+//           {appraisalGroup.map((staff) => (
+     
+//             <tr key={staff.staffId} className="warning"> 
+//               <td>{staff.staffId}</td>
+//               <td>{staff.firstname}</td>
+//               <td>{staff.lastname}</td>
+//               <td>{staff.solId}</td>
+//               <td>{staff.supervisorId}</td>
+//               <td>{staff.score}</td>
+//             </tr>
+         
+//           ))}
+//    </table>
+//    </div>
+//    </div>
+//     </>
+//   );
+// };
+
+// export default Appraisal;
+
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAppDispatch, useAppSelector } from "../../hook/hook";
 import { updateScore } from "../../Slice/AppraiseSlice";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../NavBar";
+import Swal from 'sweetalert2';
 import './Appraise.css';
 
 const Appraisal = () => {
@@ -20,19 +150,19 @@ const Appraisal = () => {
       try {
         const response = await axios.get('https://uba-outsourced.onrender.com/v1/staff');
         const AppraisedStaff = await axios.get('https://uba-outsourced.onrender.com/v1/staff/apppraisal')
-        const sqa = await axios.get('http://localhost:8000/v1/AppraisalUpload')
-        console.log('this is for appraised staff',AppraisedStaff.data.AppraisalList)
+        const sqa = await axios.get('https://uba-outsourced.onrender.com/v1/AppraisalUpload')
+        console.log('this is for appraised staff', AppraisedStaff.data.AppraisalList)
         console.log(sqa)
         const staff = response.data.staffList;
         const AppraisalList = AppraisedStaff.data.AppraisalList;
         const filteredStaff = staff.filter((item) => item.supervisorId === user.id.toUpperCase());
         const filteredAppraisal = AppraisalList.filter((item) => item.supervisorId === user.id.toUpperCase());
         const sqaAppraised = sqa.data.staffList
-            console.log('na im be this',sqaAppraised)
+        console.log('na im be this', sqaAppraised)
         setStaffList(filteredStaff)
         setAppraisalGroup(filteredAppraisal)
+        console.log(filteredAppraisal)
         setSqaapp(sqaAppraised)
-       
 
       } catch (error) {
         console.error("Error fetching staff data:", error);
@@ -42,84 +172,81 @@ const Appraisal = () => {
     fetchData();
   }, [user.id]); // Fetch data whenever user id changes
 
-// navigate to the  major appraisal page
-const handleStaffDetails = (staffId) => {
-  console.log(staffId)
-  navigate(`/staff/${staffId}`);
-};
-
-// the appraised staff 
-
-
+  // Navigate to the major appraisal page
+  const handleStaffDetails = (staffId) => {
+    const staffAlreadyAppraised = appraisalGroup.some((staff) => staff.staffId === staffId);
+    if (staffAlreadyAppraised) {
+      Swal.fire({
+        title: "Error",
+        text: "Staff has been appraised",
+        icon: "error",
+      });
+    } else {
+      console.log(staffId)
+      navigate(`/staff/${staffId}`);
+    }
+  };
 
   return (
     <>
       {/* <Navbar /> */}
       <div className="container">
         <div className="card">
-      <div className="staff pb-3">List of Staff</div>
-      <table className="table table-striped tab ">
-            <thead className="thead-dark" >
+          <div className="staff pb-3">List of Staff</div>
+          <table className="table table-striped tab">
+            <thead className="thead-dark">
               <tr>
                 <th>STAFF ID</th>
                 <th>FIRST NAME</th>
                 <th>LAST NAME</th>
                 <th>SOL ID</th>
-                
                 <th>Appraise Staff</th>
               </tr>
             </thead>
-          {staffList.map((staff) => (
-     
-            <tr key={staff.staffId} className="warning"> 
-              <td>{staff.staffId}</td>
-              <td>{staff.firstname}</td>
-              <td>{staff.lastname}</td>
-              <td>{staff.solId}</td>
-              <td>
-                <button className="btn btn-sm appbtn btn-primary" onClick={() => handleStaffDetails(staff.staffId)}>
-                  Update Score
-                </button>
+            {staffList.map((staff) => (
+              <tr key={staff.staffId} className="warning">
+                <td>{staff.staffId}</td>
+                <td>{staff.firstname}</td>
+                <td>{staff.lastname}</td>
+                <td>{staff.solId}</td>
+                <td>
+                  <button className="btn btn-sm appbtn btn-primary" onClick={() => handleStaffDetails(staff.staffId)}>
+                    Update Score
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      </div>
 
-
-              </td>
-            </tr>
-         
-          ))}
-   </table>
-   </div>
-   </div>
-
-<div className="container">
-  <div className="card">
-   <div className="staff pb-3">List of appraised Staff</div>
-      <table className="table table-striped tab ">
-            <thead className="thead-dark" >
+      <div className="container">
+        <div className="card">
+          <div className="staff pb-3">List of appraised Staff</div>
+          <table className="table table-striped tab">
+            <thead className="thead-dark">
               <tr>
                 <th>STAFF ID</th>
                 <th>FIRST NAME</th>
                 <th>LAST NAME</th>
                 <th>SOL ID</th>
                 <th>supervisorId</th>
-               
                 <th>Score</th>
               </tr>
             </thead>
-          {appraisalGroup.map((staff) => (
-     
-            <tr key={staff.staffId} className="warning"> 
-              <td>{staff.staffId}</td>
-              <td>{staff.firstname}</td>
-              <td>{staff.lastname}</td>
-              <td>{staff.solId}</td>
-              <td>{staff.supervisorId}</td>
-              <td>{staff.score}</td>
-            </tr>
-         
-          ))}
-   </table>
-   </div>
-   </div>
+            {appraisalGroup.map((staff) => (
+              <tr key={staff.staffId} className="warning">
+                <td>{staff.staffId}</td>
+                <td>{staff.firstname}</td>
+                <td>{staff.lastname}</td>
+                <td>{staff.solId}</td>
+                <td>{staff.supervisorId}</td>
+                <td>{staff.score}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      </div>
     </>
   );
 };
--- src/Component/StaffDetails/staffDetails.js
+++ src/Component/StaffDetails/staffDetails.js
@@ -1,26 +1,205 @@
+// // StaffDetails.js
+// import React, { useState } from 'react';
+// import { useQuery } from 'react-query';
+// import { useLocation, useNavigate } from 'react-router-dom';
+// import { fetchStaffDetails } from '../../API/staff';
+// import { useAppDispatch } from '../../hook/hook';
+// import Navbar from '../NavBar/index';
+// import './staffDetails.css';
+// import Form from 'react-bootstrap/Form';
+// import Swal from 'sweetalert2';
+// import axios from 'axios';
+
+// const StaffDetails = () => {
+//     const location = useLocation();
+//     const navigate = useNavigate();
+//     const staffId = location.pathname.split('/')[2];
+
+//     const [index, setIndex] = useState(0);
+//     const dispatch = useAppDispatch();
+
+//     const { data: staffDetails } = useQuery(['staffDetails', staffId], () =>
+//         fetchStaffDetails(staffId)
+//     );
+
+//     const [scores, setScores] = useState({ score1: '', score2: '', score3: '', score4: '' });
+
+//     const handleScoreChange = (name, value) => {
+//         setScores((prevScores) => ({ ...prevScores, [name]: (value) || 1 }));
+//     };
+
+//     const handleSelect = (selectedIndex) => {
+//         setIndex(selectedIndex);
+//     };
+
+//     const calculateTotalScore = async () => {
+//         const totalScore = parseInt(scores.score1) + parseInt(scores.score2) + parseInt(scores.score3) + parseInt(scores.score4);
+        
+//         const staffId = staffDetails.staffId;
+//         const firstname = staffDetails.firstname;
+//         const email = staffDetails.email;
+//         const lastname = staffDetails.lastname;
+//         const solId = staffDetails.solId;
+//         const supervisorId = staffDetails.supervisorId;
+//         const score = totalScore / 2;
+
+//         console.log('Total Score:', score);
+//         const appraisal = { staffId, firstname, lastname, email, solId, score, supervisorId };
+
+//         if (score <= 40) {
+//             Swal.fire({
+//                 title: "<h4>THIS WAS NOT SAVED</h4>",
+//                 html: "<p>The lowest a staff can get is 40</p>",
+//                 showCancelButton: true,
+//             }).then((result) => {
+//                 if (result.isDenied) {
+//                     Swal.fire("Changes are not saved", "", "info");
+//                 }
+//             });
+//         } else if (score >= 100) {
+//             Swal.fire({
+//                 title: "<h4>THIS WAS NOT SAVED</h4>",
+//                 html: "<p>The highest a staff can get is 99</p>",
+//                 showCancelButton: true,
+//             }).then((result) => {
+//                 if (result.isDenied) {
+//                     Swal.fire("Changes are not saved", "", "info");
+//                 }
+//             });
+//         } else {
+//             Swal.fire({
+//                 title: "Do you want to save this appraisal?",
+//                 showDenyButton: true,
+//                 showCancelButton: true,
+//                 confirmButtonText: "Save",
+//                 denyButtonText: `Don't save`
+//             }).then(async (result) => {
+//                 if (result.isConfirmed) {
+//                     try {
+//                         await axios.post('https://uba-outsourced.onrender.com/v1/staff/apppraisal', appraisal);
+//                         Swal.fire("Saved!", "", "success");
+//                         console.log(appraisal)
+//                         navigate(`/appraise`);
+//                     } catch (error) {
+//                         Swal.fire("Error!", "There was an error saving the appraisal.", "error");
+//                         console.error('Error posting appraisal:', error);
+//                     }
+//                 } else if (result.isDenied) {
+//                     Swal.fire("Changes are not saved", "", "info");
+//                 }
+//             });
+//         }
+//     };
+
+//     if (!staffDetails) {
+//         return <div>Loading...</div>;
+//     }
+
+//     return (
+//         <div>
+//             <Navbar />
+
+//             <div className="container-fluid px-1 py-5 mx-auto">
+//                 <div className="row d-flex justify-content-center">
+//                     <div className="card shadow-lg p-3 mb-5 bg-white">
+//                         <div className="row d-flex justify-content-between mx-2 px-3 card-strip">
+//                             <div className="left d-flex flex-column">
+//                                 <div className="mb-1 fw-bolder fs-5">Staff Details</div>
+//                             </div>
+//                         </div>
+//                         <div className="row d-flex justify-content-between mx-2 px-3 card-strip">
+//                             <div className="left d-flex flex-column">
+//                                 <div className="mb-1 fw-bold fs-7">Staff ID: {staffDetails.staffId}</div>
+//                                 <div className="mb-1 fw-bold fs-7">First Name: {staffDetails.firstname}</div>
+//                                 <div className="mb-1 fw-bold fs-7">Last Name: {staffDetails.lastname}</div>
+//                                 <div className="mb-1 fw-bold fs-7">SOL ID: {staffDetails.solId}</div>
+//                                 <div className="mb-1 fw-bold fs-7">Cost Code: {staffDetails.JobRole}</div>
+//                                 <div className="mb-1 fw-bold fs-7">Appraisal score: </div>
+//                             </div>
+//                             <div className="right d-flex">
+//                                 <div className="fa fa-comment-o"></div>
+//                                 <div className="fa fa-phone"></div>
+//                             </div>
+//                         </div>
+
+//                         <div className="left d-flex flex-column">
+//                             <div className="left d-flex flex-column">
+//                                 <div className="mb-1 fw-bolder fs-5">Appraise Staff</div>
+//                             </div>
+//                         </div>
+//                         <br />
+                        
+//                         <div className="container mb-5 pb-3">
+//                             <div className="mb-1 fw-bolder fs-9">Customer 1st Philosophy - Value Creation (Originates and contributes useful ideas; Goes the extra mile to deliver more than is expected; Solves problems)</div>
+//                             <Form.Control
+//                                 value={scores.score1}
+//                                 onChange={(e) => handleScoreChange('score1', e.target.value)}
+//                                 type="number"
+//                                 placeholder="Score (1-5)"
+//                             />
+//                         </div>
+//                         <div className="container mb-5 pb-3">
+//                             <div className="mb-1 fw-bolder fs-9">Turnaround Time (Completes assigned tasks within stipulated/expected turn-around time)</div>
+//                             <Form.Control
+//                                 value={scores.score2}
+//                                 onChange={(e) => handleScoreChange('score2', e.target.value)}
+//                                 type="number"
+//                                 placeholder="Score (1-5)"
+//                             />
+//                         </div>
+
+//                         <div className="container mb-5 pb-3">
+//                             <div className="mb-1 fw-bolder fs-9">Quality of Day-to-Day Work - Gets the work done very well; Delivers high-quality output and outcomes; Error-free work requiring minimal rework</div>
+//                             <Form.Control
+//                                 value={scores.score3}
+//                                 onChange={(e) => handleScoreChange('score3', e.target.value)}
+//                                 type="number"
+//                                 placeholder="Score (1-5)"
+//                             />
+//                         </div>
+
+//                         <div className="container mb-5 pb-3">
+//                             <div className="mb-1 fw-bolder fs-9">Productivity (Gets the job done; Has high productivity level)</div>
+//                             <Form.Control
+//                                 value={scores.score4}
+//                                 onChange={(e) => handleScoreChange('score4', e.target.value)}
+//                                 type="number"
+//                                 placeholder="Score (1-5)"
+//                             />
+//                         </div>
+//                         <div className="container mb-5 pb-3">
+//                             <button type="button" className="btn btn-primary m-2" onClick={calculateTotalScore}>
+//                                 Submit
+//                             </button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default StaffDetails;
+
+
+
 // StaffDetails.js
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { fetchStaffDetails } from '../../API/staff';
-import { useAppDispatch, useAppSelector } from '../../hook/hook';
-import { setAppraised } from '../../Slice/AppraiseSlice';
+import { useAppDispatch } from '../../hook/hook';
 import Navbar from '../NavBar/index';
 import './staffDetails.css';
-import { useNavigate } from "react-router-dom";
-import Carousel from 'react-bootstrap/Carousel';
 import Form from 'react-bootstrap/Form';
-import Appraisal from '../Appraisal_button.js/appraise';
-import { Appraised } from '../../API/authApi';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 
-const StaffDetails = ({ }) => {
+const StaffDetails = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const staffId = location.pathname.split('/')[2];
 
-    const [show, setShow] = useState(true);
     const [index, setIndex] = useState(0);
     const dispatch = useAppDispatch();
 
@@ -38,16 +217,23 @@ const StaffDetails = ({ }) => {
         setIndex(selectedIndex);
     };
 
+    const validateScores = () => {
+        return [scores.score1, scores.score2, scores.score3, scores.score4].every(
+            score => score >= 10 && score <= 24
+        );
+    };
+
     const calculateTotalScore = async () => {
+        if (!validateScores()) {
+            Swal.fire({
+                title: "Invalid Score",
+                html: "<p>Values should be between 10 and 24</p>",
+                icon: "error",
+            });
+            return;
+        }
+
         const totalScore = parseInt(scores.score1) + parseInt(scores.score2) + parseInt(scores.score3) + parseInt(scores.score4);
-        
-        const sqa = await axios.get('https://uba-outsourced.onrender.com/v1/AppraisalUpload');
-        const sqaAppraised = sqa.data.staffList;
-        const sqaapp = sqaAppraised.filter(staff => staff.staffId === staffDetails.staffId);
-        const sqatotal = sqaapp[0].Score * 100;
-        console.log('Filtered Staff Details:', sqatotal);
-
-
         
         const staffId = staffDetails.staffId;
         const firstname = staffDetails.firstname;
@@ -55,12 +241,11 @@ const StaffDetails = ({ }) => {
         const lastname = staffDetails.lastname;
         const solId = staffDetails.solId;
         const supervisorId = staffDetails.supervisorId;
-        const score = [totalScore + sqatotal] /2;
- 
+        const score = totalScore / 2;
 
         console.log('Total Score:', score);
         const appraisal = { staffId, firstname, lastname, email, solId, score, supervisorId };
-        
+
         if (score <= 40) {
             Swal.fire({
                 title: "<h4>THIS WAS NOT SAVED</h4>",
@@ -88,11 +273,17 @@ const StaffDetails = ({ }) => {
                 showCancelButton: true,
                 confirmButtonText: "Save",
                 denyButtonText: `Don't save`
-            }).then((result) => {
+            }).then(async (result) => {
                 if (result.isConfirmed) {
-                    Appraised(appraisal);
-                    Swal.fire("Saved!", "", "success");
-                    navigate(`/appraise`);
+                    try {
+                        await axios.post('https://uba-outsourced.onrender.com/v1/staff/apppraisal', appraisal);
+                        Swal.fire("Saved!", "", "success");
+                        console.log(appraisal)
+                        navigate(`/appraise`);
+                    } catch (error) {
+                        Swal.fire("Error!", "There was an error saving the appraisal.", "error");
+                        console.error('Error posting appraisal:', error);
+                    }
                 } else if (result.isDenied) {
                     Swal.fire("Changes are not saved", "", "info");
                 }
@@ -123,7 +314,7 @@ const StaffDetails = ({ }) => {
                                 <div className="mb-1 fw-bold fs-7">Last Name: {staffDetails.lastname}</div>
                                 <div className="mb-1 fw-bold fs-7">SOL ID: {staffDetails.solId}</div>
                                 <div className="mb-1 fw-bold fs-7">Cost Code: {staffDetails.JobRole}</div>
-                                <div className="mb-1 fw-bold fs-7">Appraisal score: {}</div>
+                                <div className="mb-1 fw-bold fs-7">Appraisal score: </div>
                             </div>
                             <div className="right d-flex">
                                 <div className="fa fa-comment-o"></div>
@@ -131,7 +322,6 @@ const StaffDetails = ({ }) => {
                             </div>
                         </div>
 
-                        {/* Carousel */}
                         <div className="left d-flex flex-column">
                             <div className="left d-flex flex-column">
                                 <div className="mb-1 fw-bolder fs-5">Appraise Staff</div>
